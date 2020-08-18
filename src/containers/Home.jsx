32dcb6396c5e39a18d04cae61d7b0c140e322f8f
diegoTV <diegofetv@gmail.com>
2020-08-18 18:57:30 -0500

    clase 3 Crear nuestro archivo de Rutas

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -10,7 +10,7 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initialState'
 
-const App = () => {
+const Home = () => {
   const InitialState = useInitialState(API);
   return InitialState.length === 0 ? <h1>Loading...</h1> :
     (
@@ -42,4 +42,4 @@ const App = () => {
       </div>
     );
 }
-export default App;
+export default Home;
