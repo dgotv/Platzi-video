25d2ee3f9abb89a28ebf5549e7ec1a2629703073
diegoTV <diegofetv@gmail.com>
2020-08-21 19:08:24 -0500

    clase 7 Component Layout

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import Header from '../components/Header';
+//import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
+//import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
@@ -14,8 +14,7 @@ const Home = () => {
   const InitialState = useInitialState(API);
   return InitialState.length === 0 ? <h1>Loading...</h1> :
     (
-      <div className='App'>
-        <Header />
+      <>
         <Search />
         {InitialState.mylist.length > 0 && (
           <Categories title='Mi Lista'>
@@ -37,9 +36,7 @@ const Home = () => {
           </Carousel>
         </Categories>
 
-        <Footer />
-
-      </div>
+      </>
     );
 }
 export default Home;
